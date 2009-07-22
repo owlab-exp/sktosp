0f38acf07e60d935488e290e5dba0bc544c4bef1
hunjae.lee <hunjae.lee@0b983d7e-25e1-11de-b817-d9c44aebbe61>
2009-07-22 11:19:06 +0000

    Get all friends from a user by using Criteria

---
+++ SKTOpenSocial/test/com/skt/opensocial/persistence/GetFriendsTest.java
@@ -0,0 +1,42 @@
+package com.skt.opensocial.persistence;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Transaction;
+import org.hibernate.classic.Session;
+import org.hibernate.criterion.CriteriaSpecification;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.junit.Test;
+
+public class GetFriendsTest {
+
+	@Test
+	public void getFriends() {
+		Session s = HibernateUtil.getSessionFactory().openSession();
+		Transaction t = s.beginTransaction();
+
+		String sampleUserName = "john.doe";
+
+		Criteria crit = s.createCriteria(User.class, "user")
+							.createAlias("user.friendsByMe", "fm",CriteriaSpecification.LEFT_JOIN)
+							.createAlias("user.friendsByOther", "fo",CriteriaSpecification.LEFT_JOIN)
+							.add(Restrictions
+									.or(Restrictions.eq("fm.id", sampleUserName),Restrictions.eq("fo.id", sampleUserName)));
+		// add order
+		crit.addOrder(Order.desc("user.id")); // or just "id"
+		
+		
+		List<User> friends = (List<User>) crit.list();
+
+		System.out.println("No. of friends=" + friends.size());
+		for (User u : friends)
+			System.out.println(u.getPerson().getName());
+
+		t.commit();
+		s.close();
+		HibernateUtil.getSessionFactory().close();
+	}
+
+}
